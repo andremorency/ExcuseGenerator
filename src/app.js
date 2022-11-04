33c2a35006e59600042cf88cd45bb6fac6edd899
andremorency <andremorency.genesis@gmail.com>
2022-11-04 22:45:43 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+let action = ["ate ", "peed ", "crushed ", "broke "];
+let what = ["my homework ", "the keys ", "the car "];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let WordPicker =
+  window[
+    ("who", "action", "what", "when".charAt(Math.floor(Math.random() * 4)))
+  ];
+document.write("The computer chose: " + WordPicker);
+
+function wordPicker(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let result =
+    wordPicker(who) + wordPicker(action) + wordPicker(what) + wordPicker(when);
+  document.getElementById("excuse").innerHTML = result;
+  console.log(result);
 };
+
+//   let x = Math.floor(Math.random() * who.length);
+
+//
+//   console.log(" ");
